71f97dd7d273f5d5e9ac259371fe47c647cafc6f
dgolov <dmitriy.evgenyevich@gmail.com>
2023-01-26 19:16:18 +0300

    added async

--- connect.py
+++ connect.py
@@ -1,7 +1,8 @@
-import paramiko
+import asyncio
+import asyncssh
 
 
-def connect(host_name: str, username: str, secret: str, command: str, port: str = '22') -> str:
+async def connect(host_name: str, username: str, secret: str, command: str, port: int = 22) -> str:
     """ Connect to server and run command
     :param host_name: 192.168.1.1 (For example)
     :param username: user
@@ -10,10 +11,17 @@ def connect(host_name: str, username: str, secret: str, command: str, port: str 
     :param port: ssh port
     :return: command result
     """
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=host_name, username=username, password=secret, port=port)
-    stdin, stdout, stderr = client.exec_command(command=command)
-    data = stdout.read() + stderr.read()
-    client.close()
-    return data
+    async with asyncssh.connect(host=host_name, username=username, password=secret, port=port) as conn:
+        result = await conn.run(command, check=True)
+    return result.stdout
+
+
+async def main():
+    host = ''
+    user = ''
+    password = ''
+    stdout = await connect(host, user, password, 'docker -v', 222)
+    print(stdout)
+
+if __name__ == '__main__':
+    asyncio.run(main())
