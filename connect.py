82c49b630febb865f03dd22b5ee00e33e14d8acc
dgolov <dmitriy.evgenyevich@gmail.com>
2023-01-26 19:22:01 +0300

    Updated connect result

--- connect.py
+++ connect.py
@@ -2,26 +2,32 @@ import asyncio
 import asyncssh
 
 
-async def connect(host_name: str, username: str, secret: str, command: str, port: int = 22) -> str:
+async def connect(host_name: str, username: str, secret: str, command: str, port: int = 22) -> tuple:
     """ Connect to server and run command
     :param host_name: 192.168.1.1 (For example)
     :param username: user
     :param secret: password
     :param command: docker -v
     :param port: ssh port
-    :return: command result
+    :return: host_name + command result
     """
     async with asyncssh.connect(host=host_name, username=username, password=secret, port=port) as conn:
         result = await conn.run(command, check=True)
-    return result.stdout
+    return host_name, result.stdout
 
 
 async def main():
     host = ''
     user = ''
     password = ''
-    stdout = await connect(host, user, password, 'docker -v', 222)
-    print(stdout)
+    host2 = ''
+    user2 = ''
+    password2 = ''
+
+    task1 = connect(host, user, password, 'docker -v', 222)
+    task2 = connect(host2, user2, password2, 'docker -v', 222)
+    tasks_result = await asyncio.gather(task1, task2)
+    print(tasks_result)
 
 if __name__ == '__main__':
     asyncio.run(main())
